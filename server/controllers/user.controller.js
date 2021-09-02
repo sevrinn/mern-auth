2a613cb89e911678d157c6a6640434330cf01c7a
Sevrinn <sevrinnsevrinn@gmail.com>
2021-09-01 23:27:13 -0700

    added success message

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,7 +4,7 @@ const User = require("../models/user.model");
 const register = (req, res) => {
   const { body } = req;
   User.create(req.body)
-    .then((newUser) => res.json({ newUser }))
+    .then((newUser) => res.json({ msg: "success!", newUser }))
     .catch((err) => res.status(400).json(err));
 };
 
